006bb7780a37402afcc4e8ed8f5186bba5288b83
Callil Capuozzo <callil>
2017-10-09 14:06:24 -0400

    Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Draggable from 'react-draggable'; // The default
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -11,7 +10,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      scatter: true
+      scatter: true,
+      val: 0
     }
   }
 
@@ -26,15 +26,37 @@ class App extends Component {
     window.location.href = link;
   }
 
+  wiggle = () => {
+    let val = (Math.random() * 2 - 1)*10
+    this.setState({
+      val: val
+    })
+  }
+
   render() {
     return (
       <div className="">
-        <div className={this.state.scatter ? 'info' : 'info behind'}>
+        <div className="class">
+          <div
+            onClick={this.scatter}
+            style={this.state.scatter ? {opacity: 0, transform: `translate(${(100 - (Math.random() * 2 - 1)*80)}vw, ${( 100 -(Math.random() * 2 - 1)*80)}vh)`} : {opacity: 1}} className={'x'}>
+            ×
+          </div>
+          {data.map((data, index) =>
+            <div className='person'
+              onClick={this.state.scatter ? this.scatter : (e) => this.linkOut(data.link)}
+              style={this.state.scatter ? {transform: `translate(${(70 - ((Math.random() * 2 - 1)*80)+this.state.val)}vw, ${( 70 -((Math.random() * 2 - 1)*80)+this.state.val)}vh)`} : {opacity: 1}}
+              key={index}>
+              <img className='image' src={"data/" + data.image} alt="" />
+            </div>
+          )}
+        </div>
+        <div className={this.state.scatter ? 'info' : 'info behind'} onMouseLeave={this.wiggle} onMouseEnter={this.wiggle}>
           <h1 className='title'>Super Mega Interactive</h1>
           <h3 className='title2'>SVA IxD and COMMS - Fall 2017</h3>
           <p className='links'><a href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">Running Deck ↝</a></p>
-          <p className='links people' onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>Student Work ↜</a></p>
-          <p>We are already superhuman. This course teaches yaou the skills to use digital & interaction design to intelligently and critically design for our cyborg reality. Technology is augmenting you nearly every moment of your life - interaction design is the field that gives you the tools to define how that augmentation will happen. We’ll answer questions you’ve always been asking such as: </p>
+          <p className='links people' onMouseEnter={this.wiggle} onMouseLeave={this.wiggle} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>Student Work ↜</a></p>
+          <p>We are already superhuman. This course teaches you the skills to use digital & interaction design to intelligently and critically design for our cyborg reality. Technology is augmenting you nearly every moment of your life - interaction design is the field that gives you the tools to define how that augmentation will happen. We’ll answer questions you’ve always been asking such as: </p>
           <ul>
             <li>Were stairs always the same height?</li>
             <li>Who decided the sound for an iphone notification & why?</li>
@@ -54,21 +76,6 @@ class App extends Component {
           )}
           </div>
           <br />
-        </div>
-        <div className="class">
-          <div
-            onClick={this.scatter}
-            style={this.state.scatter ? {opacity: 0, transform: `translate(${(100 - (Math.random() * 2 - 1)*80)}vw, ${( 100 -(Math.random() * 2 - 1)*80)}vh)`} : {opacity: 1}} className={'x'}>
-            ×
-          </div>
-          {data.map((data, index) =>
-            <div className='person'
-              onClick={this.state.scatter ? this.scatter : (e) => this.linkOut(data.link)}
-              style={this.state.scatter ? {transform: `translate(${(70 - (Math.random() * 2 - 1)*80)}vw, ${( 70 -(Math.random() * 2 - 1)*80)}vh)`} : {opacity: 1}}
-              key={index}>
-              <img className='image' src={"data/" + data.image} alt="" />
-            </div>
-          )}
         </div>
       </div>
     );
