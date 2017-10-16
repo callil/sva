31fd52a65cadcfe55d9b84e189e3b5e2787ced54
Barron Webster <visualwebster@gmail.com>
2017-10-16 00:56:14 -0400

    Updated Readings & CSS
    Updated readings, css, updated student card animation

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
   }
 
   scatter = () => {
-    console.log('hello');
+    console.log('scatter');
     this.setState({
       scatter: !this.state.scatter
     })
@@ -28,9 +28,14 @@ class App extends Component {
   }
 
   wiggle = () => {
-    let val = (Math.random() * 2 - 1)*10
+    let val = Math.random();
+    let height = document.getElementById("classHolder").offsetTop;
+    console.log(val);
+    console.log(document.body.scrollHeight);
+    console.log(height);
     this.setState({
-      val: val
+      val: val,
+      height: height
     })
   }
 
@@ -38,8 +43,31 @@ class App extends Component {
     return (
       <div className="">
         
-        <div className={this.state.scatter ? 'info' : 'info behind'} onMouseLeave={this.wiggle} onMouseEnter={this.wiggle}>
-          <h1 className='title'>Super <br/> Mega <br/> Interactive</h1>
+        <div className="info" onMouseLeave={this.wiggle} onMouseEnter={this.wiggle}>
+          <h1 className='title'>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>S</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>u</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>p</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>e</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>r</span>
+          <br/>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>M</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>e</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>g</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>a</span>
+          <br/> 
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>I</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>n</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>t</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>e</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>r</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>a</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>c</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>t</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>i</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>v</span>
+          <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>e</span>
+          </h1>
           <h3 className='title2'>SVA IxD and COMMS - Fall 2017</h3>
 
           <p><span className = 'highlight' >We are already superhuman.</span> This course focuses on the skills required to design intelligently and critically for our cyborg reality. Technology is augmenting you nearly every moment of your life. Interaction design is the field that lets you define how that augmentation will happen. We’ll answer questions you’ve always been asking, such as: <br/></p>
@@ -51,7 +79,8 @@ class App extends Component {
             <li>Why do some doors just suck?</li>
           </ul>
 
-          <p className='links'><a href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">If you're curious, check out our running deck ↝</a></p>
+          <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">If you're curious, check out our running deck ↝</a></p>
+          <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://github.com/callil/sva">Here's this site on Github ↝</a></p>
 
           <h3> Readings </h3>
           <div className="readings">
@@ -60,9 +89,10 @@ class App extends Component {
                 <p>{data.week}</p>
                 <ul>
                 {
-                  data.assigned.map((data2) => <li><a target="_blank" href={data2.link}>{data2.title}</a></li>)
+                  data.assigned.map((data2) => <li><a onMouseEnter={this.state.scatter ? this.wiggle : null} target="_blank" href={data2.link}>{data2.title}</a></li>)
                 }
                 </ul>
+
               </div>
             )}
 
@@ -70,15 +100,25 @@ class App extends Component {
 
           <h3>Students</h3>
 
-          <p className='links people' onMouseEnter={this.wiggle} onMouseLeave={this.wiggle} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>Student Work ↜</a></p>
+          <p className='links people' onMouseEnter={this.state.scatter ? this.wiggle : null} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>{this.state.scatter ? "Gather Students!" : "Let Students Roam Free!"}</a></p>
 
-          <div className="class">
+          <div className="class" id= "classHolder">
             {data.map((data, index) =>
-              <div className='person'
-                onClick={(e) => this.linkOut(data.link)}
-                // style={this.state.scatter ? {transform: `translate(${(70 - ((Math.random() * 2 - 1)*80)+this.state.val)}vw, ${( 70 -((Math.random() * 2 - 1)*80)+this.state.val)}vh)`} : {opacity: 1}}
+              <div className={this.state.scatter ? 'person' : 'person gathered'}
+                // onClick={(e) => this.linkOut(data.link)}
+                onMouseEnter={this.state.scatter ? this.wiggle : null}
+                style={this.state.scatter ? {
+                    left: `${((Math.random() * 2 - 1) * 30)}vw`,
+                    bottom:  `${Math.random()*document.body.scrollHeight - this.state.height/3}px`,
+                    transform: `rotate(${(Math.random() * 2 - 1) * 180}deg)`
+                  } 
+                  : {
+                    left: '0px',
+                    bottom: '0px',
+                    transform: 'rotate(0deg)'
+                  }}
                 key={index}>
-                <img className='image' src={"data/" + data.image} alt="" />
+                <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
               </div>
             )}
           </div>
@@ -87,9 +127,9 @@ class App extends Component {
             <div className="teach">
             {us.map((data, index) =>
               <div className='person teacher'
-                onClick={(e) => this.linkOut(data.link)}
+                // onClick={(e) => this.linkOut(data.link)}
                 key={index}>
-                <img className='image' src={"data/" + data.image} alt="" />
+                <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
               </div>
             )}
           </div>
