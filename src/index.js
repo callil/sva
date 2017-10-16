2265572745da6da6b16282d0281a333187152852
Barron Webster <visualwebster@gmail.com>
2017-10-15 21:37:33 -0400

    Got readings working (sketchily)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import data from './data.json'
 import us from './teachers.json'
+import readings from './readings.json'
 
 class App extends Component {
   constructor(props) {
@@ -36,27 +37,12 @@ class App extends Component {
   render() {
     return (
       <div className="">
-        <div className="class">
-          <div
-            onClick={this.scatter}
-            style={this.state.scatter ? {opacity: 0, transform: `translate(${(100 - (Math.random() * 2 - 1)*80)}vw, ${( 100 -(Math.random() * 2 - 1)*80)}vh)`} : {opacity: 1}} className={'x'}>
-            ×
-          </div>
-          {data.map((data, index) =>
-            <div className='person'
-              onClick={this.state.scatter ? this.scatter : (e) => this.linkOut(data.link)}
-              style={this.state.scatter ? {transform: `translate(${(70 - ((Math.random() * 2 - 1)*80)+this.state.val)}vw, ${( 70 -((Math.random() * 2 - 1)*80)+this.state.val)}vh)`} : {opacity: 1}}
-              key={index}>
-              <img className='image' src={"data/" + data.image} alt="" />
-            </div>
-          )}
-        </div>
+        
         <div className={this.state.scatter ? 'info' : 'info behind'} onMouseLeave={this.wiggle} onMouseEnter={this.wiggle}>
-          <h1 className='title'>Super Mega Interactive</h1>
+          <h1 className='title'>Super <br/> Mega <br/> Interactive</h1>
           <h3 className='title2'>SVA IxD and COMMS - Fall 2017</h3>
-          <p className='links'><a href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">Running Deck ↝</a></p>
-          <p className='links people' onMouseEnter={this.wiggle} onMouseLeave={this.wiggle} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>Student Work ↜</a></p>
-          <p>We are already superhuman. This course teaches you the skills to use digital & interaction design to intelligently and critically design for our cyborg reality. Technology is augmenting you nearly every moment of your life - interaction design is the field that gives you the tools to define how that augmentation will happen. We’ll answer questions you’ve always been asking such as: </p>
+
+          <p><span className = 'highlight' >We are already superhuman.</span> This course focuses on the skills required to design intelligently and critically for our cyborg reality. Technology is augmenting you nearly every moment of your life. Interaction design is the field that lets you define how that augmentation will happen. We’ll answer questions you’ve always been asking, such as: <br/></p>
           <ul>
             <li>Were stairs always the same height?</li>
             <li>Who decided the sound for an iphone notification & why?</li>
@@ -64,19 +50,51 @@ class App extends Component {
             <li>Why is the A next to the S on your keyboard?</li>
             <li>Why do some doors just suck?</li>
           </ul>
-          <p>Check out the links above to see our running deck that includes current and past assignments and reading.</p>
-          <div>Teachers:</div>
-          <div className="teach">
-          {us.map((data, index) =>
-            <div className='teachers'
-              onClick={(e) => this.linkOut(data.link)}
-              key={index}>
-              <img className='image' src={"data/" + data.image} alt="" />
-            </div>
-          )}
+
+          <p className='links'><a href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">If you're curious, check out our running deck ↝</a></p>
+
+          <h3> Readings </h3>
+          <div className="readings">
+            {readings.map((data) =>
+              <div className='week'>
+                <p>{data.week}</p>
+                <ul>
+                {
+                  data.assigned.map((data2) => <li><a target="_blank" href={data2.link}>{data2.title}</a></li>)
+                }
+                </ul>
+              </div>
+            )}
+
           </div>
-          <br />
+
+          <h3>Students</h3>
+
+          <p className='links people' onMouseEnter={this.wiggle} onMouseLeave={this.wiggle} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>Student Work ↜</a></p>
+
+          <div className="class">
+            {data.map((data, index) =>
+              <div className='person'
+                onClick={(e) => this.linkOut(data.link)}
+                // style={this.state.scatter ? {transform: `translate(${(70 - ((Math.random() * 2 - 1)*80)+this.state.val)}vw, ${( 70 -((Math.random() * 2 - 1)*80)+this.state.val)}vh)`} : {opacity: 1}}
+                key={index}>
+                <img className='image' src={"data/" + data.image} alt="" />
+              </div>
+            )}
+          </div>
+
+          <h3>Taught by:</h3>
+            <div className="teach">
+            {us.map((data, index) =>
+              <div className='person teacher'
+                onClick={(e) => this.linkOut(data.link)}
+                key={index}>
+                <img className='image' src={"data/" + data.image} alt="" />
+              </div>
+            )}
+          </div>
         </div>
+        
       </div>
     );
   }
