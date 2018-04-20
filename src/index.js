2d3d9b0c1dde10cbe74ff6c95e2bd266da8686c4
Callil Capuozzo <callil.capuozzo@gmail.com>
2018-04-20 13:41:20 -0400

    Fill out readings

--- src/index.js
+++ src/index.js
@@ -68,72 +68,79 @@ class App extends Component {
           <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>v</span>
           <span className="letter" onMouseEnter={this.state.scatter ? this.wiggle : null}>e</span>
           </h1>
-          <h3 className='title2'>SVA IxD and COMMS - Fall 2017</h3>
 
-          <p><span className = 'highlight' >We are already superhuman.</span> This course focuses on the skills required to design intelligently and critically for our cyborg reality. Technology is augmenting you nearly every moment of your life. Interaction design is the field that lets you define how that augmentation will happen. We’ll answer questions you’ve always been asking, such as: <br/></p>
-          <ul>
-            <li>Were stairs always the same height?</li>
-            <li>Who decided the sound for an iphone notification & why?</li>
-            <li>What would happen if we used a computer with a mouse in BOTH your hands???</li>
-            <li>Why is the A next to the S on your keyboard?</li>
-            <li>Why do some doors just suck?</li>
-          </ul>
+          <div className="flex-collapse">
+            <div>
+              <h3 className='title2'>SVA IxD and COMMS - Fall 2017</h3>
 
-          <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">If you're curious, check out our running deck ↝</a></p>
-          <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://github.com/callil/sva">Here's this site on Github ↝</a></p>
+              <p><span className = 'highlight' >We are already superhuman.</span> This course focuses on the skills required to design intelligently and critically for our cyborg reality. Technology is augmenting you nearly every moment of your life. Interaction design is the field that lets you define how that augmentation will happen. We’ll answer questions you’ve always been asking, such as: <br/></p>
+              <ul>
+                <li>Were stairs always the same height?</li>
+                <li>Who decided the sound for an iphone notification & why?</li>
+                <li>What would happen if we used a computer with a mouse in BOTH your hands???</li>
+                <li>Why is the A next to the S on your keyboard?</li>
+                <li>Why do some doors just suck?</li>
+              </ul>
 
-          <h3> Readings </h3>
-          <div className="readings">
-            {readings.map((data) =>
-              <div className='week'>
-                <p>{data.week}</p>
-                <ul>
-                {
-                  data.assigned.map((data2) => <li><a onMouseEnter={this.state.scatter ? this.wiggle : null} target="_blank" href={data2.link}>{data2.title}</a></li>)
-                }
-                </ul>
+              <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://docs.google.com/presentation/d/1Y0XQ2Xcesa27cf3Bo_4w1dwUWtJl1RiwGEBYbU8y6WY/edit">If you're curious, check out our running deck ↝</a></p>
+              <p className='links'><a onMouseEnter={this.state.scatter ? this.wiggle : null} href="https://github.com/callil/sva">Here's this site on Github ↝</a></p>
+            </div>
+            <div>
+              <h3> Readings </h3>
+              <div className="readings">
+                {readings.map((data) =>
+                  <div className='week'>
+                    <p>{data.week}</p>
+                    <ul>
+                    {
+                      data.assigned.map((data2) => <li><a onMouseEnter={this.state.scatter ? this.wiggle : null} target="_blank" href={data2.link}>{data2.title}</a></li>)
+                    }
+                    </ul>
+
+                  </div>
+                )}
 
               </div>
-            )}
+            </div>
+            <div className="students">
+            {/* <h3>Students</h3> */}
 
+            <p className='links people' onMouseEnter={this.state.scatter ? this.wiggle : null} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>{this.state.scatter ? "Gather Students!" : "Let Students Roam Free!"}</a></p>
+
+            <div className="class" id= "classHolder">
+              {data.map((data, index) =>
+                <div className={this.state.scatter ? 'person' : 'person gathered'}
+                  // onClick={(e) => this.linkOut(data.link)}
+                  onMouseEnter={this.state.scatter ? this.wiggle : null}
+                  style={this.state.scatter ? {
+                      left: `${((Math.random() * 2 - 1) * 30)}vw`,
+                      bottom:  `${Math.random()*document.body.scrollHeight - this.state.height/3}px`,
+                      transform: `rotate(${(Math.random() * 2 - 1) * 180}deg)`
+                    } 
+                    : {
+                      left: '0px',
+                      bottom: '0px'
+                    }}
+                  key={index}>
+                  <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
+                </div>
+              )}
+            </div>
+            </div>
+            <div>
+            <h3>Taught by:</h3>
+              <div className="teach">
+              {us.map((data, index) =>
+                <div className='person teacher'
+                  // onClick={(e) => this.linkOut(data.link)}
+                  key={index}>
+                  <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
+                </div>
+              )}
+            </div>
           </div>
-
-          <h3>Students</h3>
-
-          <p className='links people' onMouseEnter={this.state.scatter ? this.wiggle : null} onClick={this.scatter}><a href="" onClick={(e) => e.preventDefault()}>{this.state.scatter ? "Gather Students!" : "Let Students Roam Free!"}</a></p>
-
-          <div className="class" id= "classHolder">
-            {data.map((data, index) =>
-              <div className={this.state.scatter ? 'person' : 'person gathered'}
-                // onClick={(e) => this.linkOut(data.link)}
-                onMouseEnter={this.state.scatter ? this.wiggle : null}
-                style={this.state.scatter ? {
-                    left: `${((Math.random() * 2 - 1) * 30)}vw`,
-                    bottom:  `${Math.random()*document.body.scrollHeight - this.state.height/3}px`,
-                    transform: `rotate(${(Math.random() * 2 - 1) * 180}deg)`
-                  } 
-                  : {
-                    left: '0px',
-                    bottom: '0px'
-                  }}
-                key={index}>
-                <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
-              </div>
-            )}
-          </div>
-
-          <h3>Taught by:</h3>
-            <div className="teach">
-            {us.map((data, index) =>
-              <div className='person teacher'
-                // onClick={(e) => this.linkOut(data.link)}
-                key={index}>
-                <a href={data.link} target="_blank"><img className='image' src={"data/" + data.image} alt="" /></a>
-              </div>
-            )}
           </div>
         </div>
-        
       </div>
     );
   }
